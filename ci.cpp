c955ecf1cffbb79a6e374d0f1bde8bffc90c6982
red <postred2@protonmail.com>
2024-11-28 20:37:36 +0200

    windows sdk

--- ci.cpp
+++ ci.cpp
@@ -1,8 +1,10 @@
-﻿
+﻿#include <winmeta.h>
+#include "span"
+#include <iostream>
+
 int main()
 {
-	while (true) {
-
-	}
+	std::span<float> x{};
+	std::cout << MSG_category_Disk;
 	return 0;//never
 }
